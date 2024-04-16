6b7ad6bdeb14ef0c2fe219c801b8cc80c20f5443
alcun <alasdair@talkwell.world>
2024-04-17 00:51:18 +0100

    progress ... ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import expressiveCode from "astro-expressive-code";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [expressiveCode(), icon()]
+  integrations: [expressiveCode(), icon(), sitemap()]
 });
